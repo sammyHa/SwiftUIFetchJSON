42cd60fa1b151d3e2bba5fd8959243912a62cc2a
samimhakimi <samimgooogl@gmail.com>
2021-05-04 11:33:20 -0400

    Initial Commit

---
+++ JsonDataUrlSession/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JsonDataUrlSession
+//
+//  Created by Samim Hakimi on 5/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JsonDataUrlSession/JsonDataUrlSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonDataUrlSessionApp.swift
+//  JsonDataUrlSession
+//
+//  Created by Samim Hakimi on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct JsonDataUrlSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
